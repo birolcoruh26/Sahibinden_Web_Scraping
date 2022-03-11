cc8a7d19cf501c05a1582671335b324a042a2f0d
Birol Coruh <65693604+birolcoruh26@users.noreply.github.com>
2022-03-11 23:07:13 +0300

    Final

--- Sanction_Task/Program.cs
+++ Sanction_Task/Program.cs
@@ -9,24 +9,25 @@ namespace Sanction_Task
 {
     internal class Program
     {
-       static HtmlAgilityPack.HtmlDocument? html;
-       static List<string> postLinksForDetail = new List<string>();
-       static List<string> postNames = new List<string>();
-       static List<string> postPrices = new List<string>();
-       static List<int> priceForMean = new List<int>();
+        static Dictionary<string, string> prime = new Dictionary<string, string>();
+        static HtmlAgilityPack.HtmlDocument? html;
+        static List<string> postLinksForDetail = new List<string>();
+        static List<string> postNames = new List<string>();
+        static List<string> postPrices = new List<string>();
+        static List<int> pricesForMeans = new List<int>();
         static void Main(string[] args)
         {
             ReadDetailLinks("https://www.sahibinden.com");
             ReadDetails("https://www.sahibinden.com");
             ShowInConsole();
-            SaveToFile();
-        }  
+            SaveToFile(@"C:");
+        }
         public static void ReadDetailLinks(string link)
         {
             string pageSource;
-            
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(link); 
-            request.Timeout = 1000;
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(link);
+            request.Timeout = 3000;
             WebResponse response = request.GetResponse();
             using (Stream responseStream = response.GetResponseStream())
             {
@@ -42,7 +43,7 @@ namespace Sanction_Task
                 HtmlNode[] nodes = html.DocumentNode.SelectNodes(@"//*[@id=""container""]/ div[3]/div/div[3]/div[3]/ul/li/a").ToArray();
                 foreach (var item in nodes)
                 {
-                    if(item.Attributes["href"].Value.Contains("/ilan")) 
+                    if (item.Attributes["href"].Value.Contains("/ilan"))
                     {
                         postLinksForDetail.Add(item.Attributes["href"].Value);
                         postNames.Add(item.InnerText);
@@ -51,16 +52,16 @@ namespace Sanction_Task
                     {
                         Console.WriteLine("burası reklam olan satır");
                     }
-                } 
+                }
             }
         }
         public static void ReadDetails(string link)
         {
             string pageSourceForDetails;
-            for (int i = 0; i < postLinksForDetail.Count; i++)
+            for (int i = 0; i < 3; i++)
             {
                 HttpWebRequest requestForDetail = (HttpWebRequest)WebRequest.Create(link + postLinksForDetail[i].ToString()); //linki detay linki ile birleştirdim.
-                requestForDetail.Timeout = 1000;
+                requestForDetail.Timeout = 3000;
                 WebResponse responseForDetail = requestForDetail.GetResponse();
                 using (Stream responseForDetailStream = responseForDetail.GetResponseStream())
                 {
@@ -70,48 +71,55 @@ namespace Sanction_Task
                 using (WebClient clientForDetail = new WebClient())
                 {
                     clientForDetail.Headers.Add("user-agent", Guid.NewGuid().ToString());
-                    clientForDetail.Proxy = new WebProxy("89.43.31.134"); 
+                    clientForDetail.Proxy = new WebProxy("89.43.31.134");
                     html = new HtmlAgilityPack.HtmlDocument();
                     html.LoadHtml(pageSourceForDetails);
                     var nodesForDetail = html.DocumentNode.SelectSingleNode(@"//*[@id=""favoriteClassifiedPrice""]"); //*[@id="favoriteClassifiedPrice"]
-                    if(nodesForDetail == null)
+                    if (nodesForDetail == null)
                     {
                         Console.WriteLine("Burası Reklam Satırı");
                     }
                     else
                     {
                         postPrices.Add(nodesForDetail.Attributes["value"].Value);
-                        postPrices[i].Replace("TL", "");   
+                        postPrices[i] = postPrices[i].Replace("TL", "");
+                        postPrices[i] = postPrices[i].Replace(".", "");
                     }
                 }
             }
- // Not: vitrinde 56 veri var ve 3 adedi reklam olduğu için 53 adet veri geliyor. Fakat Sahibinden.com veri çektikten belli bir süre 
- // Sonra ip ban atıyor. Bu nedenden ötürü ortalama 6 - 9 veri aldıktan sonra System.Net.WebException: 'The remote server returned an error:(429)
- // hatası veriyor. Bu kısmı çözmek için timeout, proxy, useragent gibi yollar denedim fakat yapamadım. Değerlendirmeniz için belirtmek istedim.
+            // Not: vitrinde 56 veri var ve 3 adedi reklam olduğu için 53 adet veri geliyor. Fakat Sahibinden.com veri çektikten belli bir süre 
+            // Sonra ip ban atıyor. 3-4 veri ile denediğimde oldu fakat 56 verinin tamamı ile deneyince System.Net.WebException: 'The remote server returned an error:(429)
+            // hatası veriyor. Bu kısmı çözmek için timeout, proxy, useragent gibi yollar denedim fakat yapamadım. Değerlendirmeniz için belirtmek istedim.
         }
         public static void ShowInConsole()
         {
             for (int i = 0; i < postPrices.Count; i++)
             {
                 Console.WriteLine(postNames[i] + ":" + postPrices[i]);
+                prime.Add(postNames[i], postPrices[i]);
             }
-            if(postPrices.Count > 0)
+            if (postPrices.Count > 0)
             {
                 foreach (var item in postPrices)
                 {
-                    priceForMean.Add(Convert.ToInt32(item));
+                    pricesForMeans.Add(Convert.ToInt32(item));
                 }
             }
-            Console.WriteLine(priceForMean.Average().ToString());
-            Console.ReadLine();
+            Console.WriteLine("mean:"+pricesForMeans.Average().ToString());
         }
-        public static void SaveToFile()
+        public static void SaveToFile(string path)
         {
-            List<string> primeList = new List<string>();
-            primeList = postNames.Concat(postPrices).ToList();
-            System.IO.File.WriteAllLines(@"C:\Users\birol\OneDrive\Belgeler\Sahibinden.txt", primeList);
+            FileStream fs = File.Create(path + "\\" + "sahibinden.txt");
+            StreamWriter sw = new StreamWriter(fs);
+            foreach (var item in prime)
+            {
+                sw.WriteLine(item.Key.Trim() + ":" + item.Value.Trim());
+            }
+            sw.WriteLine("mean:" + pricesForMeans.Average().ToString()); //ortalama
+            sw.Close();
+            fs.Close();
         }
-     
+
     }
     
 }
