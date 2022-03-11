8bd08995a088f3bcd6dbf9d0bc5f690bc1540f85
Birol Coruh <65693604+birolcoruh26@users.noreply.github.com>
2022-03-11 19:31:46 +0300

    Showing on Console Added

--- Sanction_Task/Program.cs
+++ Sanction_Task/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Text;
 using HtmlAgilityPack;
@@ -7,15 +8,20 @@ namespace Sanction_Task
 {
     internal class Program
     {
-        
+        HtmlAgilityPack.HtmlDocument? html;
+       static List<string> postLinksForDetail = new List<string>();
+       static List<string> postNames = new List<string>();
+       static List<string> postPrices = new List<string>();
+
         static void Main(string[] args)
         {
+            ShowInConsole();
+        }  
+        public void ReadDetailLinks(string link)
+        {
             string pagesource;
-            List<string> postLinksForDetail = new List<string>();
-            List<string> postNames = new List<string>();
-            List<string> postPrices = new List<string>();
-            HtmlAgilityPack.HtmlDocument html;
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://www.sahibinden.com/");
+            
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(link); /*https://www.sahibinden.com/anasayfa-vitrin?viewType=Gallery*/
             WebResponse response = request.GetResponse();
             using (Stream responseStream = response.GetResponseStream())
             {
@@ -24,42 +30,52 @@ namespace Sanction_Task
             }
             using (WebClient client = new WebClient())
             {
-               
+                client.Proxy = new WebProxy("89.43.31.134");
                 html = new HtmlAgilityPack.HtmlDocument();
                 html.LoadHtml(pagesource);
                 HtmlNode[] nodes = html.DocumentNode.SelectNodes(@"//*[@id=""container""]/ div[3]/div/div[3]/div[3]/ul/li/a").ToArray();
                 foreach (var item in nodes)
                 {
-                   postLinksForDetail.Add(item.Attributes["href"].Value);
-                   postNames.Add(item.InnerText);
-                   
+                    postLinksForDetail.Add(item.Attributes["href"].Value);
+                    postNames.Add(item.InnerText);
                 }
             }
+        }
+        public void ReadDetails(string link)
+        {
+            string pageSourceForDetails;
             for (int i = 0; i < postLinksForDetail.Count; i++)
             {
-                HttpWebRequest requestForDetail = (HttpWebRequest)WebRequest.Create($"https://www.sahibinden.com" + postLinksForDetail[i].ToString());
+                HttpWebRequest requestForDetail = (HttpWebRequest)WebRequest.Create(link + postLinksForDetail[i].ToString());
                 WebResponse responseForDetail = requestForDetail.GetResponse();
                 using (Stream responseForDetailStream = responseForDetail.GetResponseStream())
                 {
                     StreamReader reader = new StreamReader(responseForDetailStream, encoding: Encoding.UTF8);
-                    pagesource = reader.ReadToEnd();
+                    pageSourceForDetails = reader.ReadToEnd();
                 }
                 using (WebClient clientForDetail = new WebClient())
                 {
+                    clientForDetail.Proxy = new WebProxy("89.43.31.134"); //sahibinden ip ban atıyor onu engellemek için koydum.
                     html = new HtmlAgilityPack.HtmlDocument();
-                    html.LoadHtml(pagesource);
+                    html.LoadHtml(pageSourceForDetails);
                     var nodesForDetail = html.DocumentNode.SelectSingleNode(@"//*[@id=""favoriteClassifiedPrice""]"); //*[@id="favoriteClassifiedPrice"]
                     postPrices.Add(nodesForDetail.Attributes["value"].Value);
-
+                    
                 }
             }
-
+        }
+        public static void ShowInConsole()
+        {
+            for (int i = 0; i < postPrices.Count; i++)
+            {
+                Console.WriteLine(postNames[i] + ":" + postPrices[i]);
+            }
             Console.ReadLine();
-
+        }
+        public void MeanPrice()
+        {
 
         }
-        
-        
     }
     
 }
