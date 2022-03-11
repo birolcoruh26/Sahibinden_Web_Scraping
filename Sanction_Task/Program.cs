fa17db86538a5bde00f60bdbb1e4aa347b51e63a
Birol Coruh <65693604+birolcoruh26@users.noreply.github.com>
2022-03-11 21:48:54 +0300

    SaveToFile Added

--- Sanction_Task/Program.cs
+++ Sanction_Task/Program.cs
@@ -3,50 +3,64 @@ using System.Collections.Generic;
 using System.Net;
 using System.Text;
 using HtmlAgilityPack;
-
+using System.IO;
+using System.Configuration;
 namespace Sanction_Task
 {
     internal class Program
     {
-        HtmlAgilityPack.HtmlDocument? html;
+       static HtmlAgilityPack.HtmlDocument? html;
        static List<string> postLinksForDetail = new List<string>();
        static List<string> postNames = new List<string>();
        static List<string> postPrices = new List<string>();
-
+       static List<int> priceForMean = new List<int>();
         static void Main(string[] args)
         {
+            ReadDetailLinks("https://www.sahibinden.com");
+            ReadDetails("https://www.sahibinden.com");
             ShowInConsole();
+            SaveToFile();
         }  
-        public void ReadDetailLinks(string link)
+        public static void ReadDetailLinks(string link)
         {
-            string pagesource;
+            string pageSource;
             
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(link); /*https://www.sahibinden.com/anasayfa-vitrin?viewType=Gallery*/
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(link); 
+            request.Timeout = 1000;
             WebResponse response = request.GetResponse();
             using (Stream responseStream = response.GetResponseStream())
             {
                 StreamReader reader = new StreamReader(responseStream, encoding: Encoding.UTF8);
-                pagesource = reader.ReadToEnd();
+                pageSource = reader.ReadToEnd();
             }
             using (WebClient client = new WebClient())
             {
+                client.Headers.Add("user-agent", Guid.NewGuid().ToString());
                 client.Proxy = new WebProxy("89.43.31.134");
                 html = new HtmlAgilityPack.HtmlDocument();
-                html.LoadHtml(pagesource);
+                html.LoadHtml(pageSource);
                 HtmlNode[] nodes = html.DocumentNode.SelectNodes(@"//*[@id=""container""]/ div[3]/div/div[3]/div[3]/ul/li/a").ToArray();
                 foreach (var item in nodes)
                 {
-                    postLinksForDetail.Add(item.Attributes["href"].Value);
-                    postNames.Add(item.InnerText);
-                }
+                    if(item.Attributes["href"].Value.Contains("/ilan")) 
+                    {
+                        postLinksForDetail.Add(item.Attributes["href"].Value);
+                        postNames.Add(item.InnerText);
+                    }
+                    else   // ilan olmayan 3 adet girdiyi burada eliyorum bu 3 veri reklam oluyor.
+                    {
+                        Console.WriteLine("burası reklam olan satır");
+                    }
+                } 
             }
         }
-        public void ReadDetails(string link)
+        public static void ReadDetails(string link)
         {
             string pageSourceForDetails;
             for (int i = 0; i < postLinksForDetail.Count; i++)
             {
-                HttpWebRequest requestForDetail = (HttpWebRequest)WebRequest.Create(link + postLinksForDetail[i].ToString());
+                HttpWebRequest requestForDetail = (HttpWebRequest)WebRequest.Create(link + postLinksForDetail[i].ToString()); //linki detay linki ile birleştirdim.
+                requestForDetail.Timeout = 1000;
                 WebResponse responseForDetail = requestForDetail.GetResponse();
                 using (Stream responseForDetailStream = responseForDetail.GetResponseStream())
                 {
@@ -55,14 +69,25 @@ namespace Sanction_Task
                 }
                 using (WebClient clientForDetail = new WebClient())
                 {
-                    clientForDetail.Proxy = new WebProxy("89.43.31.134"); //sahibinden ip ban atıyor onu engellemek için koydum.
+                    clientForDetail.Headers.Add("user-agent", Guid.NewGuid().ToString());
+                    clientForDetail.Proxy = new WebProxy("89.43.31.134"); 
                     html = new HtmlAgilityPack.HtmlDocument();
                     html.LoadHtml(pageSourceForDetails);
                     var nodesForDetail = html.DocumentNode.SelectSingleNode(@"//*[@id=""favoriteClassifiedPrice""]"); //*[@id="favoriteClassifiedPrice"]
-                    postPrices.Add(nodesForDetail.Attributes["value"].Value);
-                    
+                    if(nodesForDetail == null)
+                    {
+                        Console.WriteLine("Burası Reklam Satırı");
+                    }
+                    else
+                    {
+                        postPrices.Add(nodesForDetail.Attributes["value"].Value);
+                        postPrices[i].Replace("TL", "");   
+                    }
                 }
             }
+ // Not: vitrinde 56 veri var ve 3 adedi reklam olduğu için 53 adet veri geliyor. Fakat Sahibinden.com veri çektikten belli bir süre 
+ // Sonra ip ban atıyor. Bu nedenden ötürü ortalama 6 - 9 veri aldıktan sonra System.Net.WebException: 'The remote server returned an error:(429)
+ // hatası veriyor. Bu kısmı çözmek için timeout, proxy, useragent gibi yollar denedim fakat yapamadım. Değerlendirmeniz için belirtmek istedim.
         }
         public static void ShowInConsole()
         {
@@ -70,12 +95,23 @@ namespace Sanction_Task
             {
                 Console.WriteLine(postNames[i] + ":" + postPrices[i]);
             }
+            if(postPrices.Count > 0)
+            {
+                foreach (var item in postPrices)
+                {
+                    priceForMean.Add(Convert.ToInt32(item));
+                }
+            }
+            Console.WriteLine(priceForMean.Average().ToString());
             Console.ReadLine();
         }
-        public void MeanPrice()
+        public static void SaveToFile()
         {
-
+            List<string> primeList = new List<string>();
+            primeList = postNames.Concat(postPrices).ToList();
+            System.IO.File.WriteAllLines(@"C:\Users\birol\OneDrive\Belgeler\Sahibinden.txt", primeList);
         }
+     
     }
     
 }
